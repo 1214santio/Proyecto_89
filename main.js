9d2bb29de7511f916f75cd1c79e8fe54000b55e8
1214santio <98626565+1214santio@users.noreply.github.com>
2022-06-06 16:01:01 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function adduser()
 {
-    
+    var NombreJugador1 = document.getElementById("nombre1").value;
+    var NombreJugador2 = document.getElementById("nombre2").value;
+    localStorage.setItem("Nombre del jugador 1", NombreJugador1);
+    localStorage.setItem("Nombre del jugador 2", NombreJugador2);
+    window.location = "index2.html";
 }
