0657d8834384b665202362c5b36b953635e8377d
1214santio <98626565+1214santio@users.noreply.github.com>
2022-06-20 19:21:51 -0500

    Add files via upload

--- main2.js
+++ main2.js
@@ -1,3 +1,26 @@
+NombreJugador1 = localStorage.getItem("Nombre del jugador 1")
+NombreJugador2 = localStorage.getItem("Nombre del jugador 2")
+update_player1_score = 0;
+player1_score = 0;
+update_player2_score = 0;
+player2_score = 0;
+document.getElementById("player1_name").innerHTML = localStorage.getItem("Nombre del jugador 1")
+document.getElementById("player2_name").innerHTML = localStorage.getItem("Nombre del jugador 2")
+document.getElementById("TurnoParaPreguntar").innerHTML = "Turno para preguntar - " + localStorage.getItem("Nombre del jugador 1")
+document.getElementById("TurnoParaResponder").innerHTML = "Turno para responder - " + localStorage.getItem("Nombre del jugador 2")
+function send()
+{
+    number1 = document.getElementById("number_1").value;
+    number2 = document.getElementById("number_2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+    question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
+    input_box = "<br>Respuesta: <input type = 'text' id = 'input_check_box'>";
+    check_button = "<br><br><button class = 'btn btn-info' onclick = 'check()'>Comprobar</button>";
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number_1").value = "";
+    document.getElementById("number_2").value = "";
+}
 document.getElementById("player1_name").innerHTML = localStorage.getItem("Nombre del jugador 1")
 document.getElementById("player2_name").innerHTML = localStorage.getItem("Nombre del jugador 2")
 document.getElementById("TurnoParaPreguntar").innerHTML = "Turno para preguntar - " + localStorage.getItem("Nombre del jugador 1")
